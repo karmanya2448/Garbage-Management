127c74fb6ee0a636caf956a86942cb98e88c13a1
Karmanya Kumra <karmanya.kumra@gmail.com>
2025-11-24 12:11:36 +0530

    Delete Garbage_Management-main directory

--- Garbage_Management-main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from app1.models import *
-from models import user_details
-# Register your models here.
-admin.site.register(user_details)
-admin.site.register(driver_info)
-admin.site.register(garbageDetails)
-admin.site.register(schedule)
